d772d2a96bcf8a074aba12c0df19d34acce38a1b
tudu74 <52302344+tudu74@users.noreply.github.com>
2020-03-25 07:59:30 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+
+    def __init__(self):
+
+        self.screen_width = 800
+        self.screen_height = 600
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.red = (255, 0, 0)
+        self.green = (0, 255, 0)
+        self.blue = (0, 0, 255)
+        self.FPS = 90
